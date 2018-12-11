c7e042514214f81601089002384e24694d51bb68
pyongie210 <41872735+pyongie210@users.noreply.github.com>
2018-12-11 08:26:05 +0700

    Update Obstacle.h

--- Obstacle.h
+++ Obstacle.h
@@ -8,20 +8,54 @@ class Obstacle {
 private:
 	int mX, mY;
 	int counter;
+	int direction =1; //UPDATE
+
 
 public:
-	Obstacle(int x, int y);
+	Obstacle(int x, int y, int z); //UPDATE
 	Obstacle(ifstream& fin);
 
 	int getMX();
 	int getMY();
 
-	void inc();
+	void inc(); //counter
 
 	bool isTime();
 
-	void Move(bool move);
+	void Move(bool move); //UPDATE
 
 };
 
+class Truck : public Obstacle {
+public:
+	Truck(int x, int y, int z) : Obstacle(x, y, z)
+	{
+	};
+};
+
+class Car : public Obstacle {
+public:
+	Car(int x, int y, int z) : Obstacle(x, y, z)
+	{
+	};
+};
+
+class Dino : public Obstacle {
+public:
+	Dino(int x, int y, int z) : Obstacle(x, y, z)
+{
+};
+};
+
+class Bird : public Obstacle {
+public:
+	Bird(int x, int y, int z) : Obstacle(x, y, z)
+{
+};
+};
+
+class Factory
+{
+public:	vector<Obstacle*> portion(int x, int type); //create line of obstacle
+};
 #endif
